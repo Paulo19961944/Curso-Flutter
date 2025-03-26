5c5dc894dc4f49e9aa1dbd19158edeff1b942891
Paulo19961944 <paulo24041996@gmail.com>
2025-03-26 15:46:53 -0300

    [feat] Adicionando Row (Add Row)

--- scaffold_project/lib/main.dart
+++ scaffold_project/lib/main.dart
@@ -1,17 +1,24 @@
+// Importa o Material Design
 import 'package:flutter/material.dart';
 
-void main(){
+// Função Principal
+void main() {
   runApp(MyApp());
 }
 
+// Classe MyApp como Stateful Widget
 class MyApp extends StatefulWidget {
+  // Construtor da Superclasse
   const MyApp({super.key});
 
+  // Sobreescreve o State com o MyAppState
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
+// Cria uma Classe Privada MyAppState
 class _MyAppState extends State<MyApp> {
+  // Cria o Widget que será exibido na tela e Sobreescreve o método Build
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,11 +34,24 @@ class _MyAppState extends State<MyApp> {
             ),
           ),
         ),
-        body: const Center(
-          child: Text(
-            'This is a Scaffold'
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+          Icon(
+            Icons.phone,
+            size: 32,
           ),
-        ), 
+          Padding(
+              padding: EdgeInsets.only(left: 8),
+              child: Center(
+                child: Text(
+                  'Meu Aplicativo'
+                ),
+              ),
+          ),
+        ],
+        ),
       ),
     );
   }
